43b508a17d738aa09e1c6d7810a9753cb728af29
Amit Kothari <amitkothari910@gmail.com>
2025-09-02 02:39:15 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Optional: add global prefix for APIs
-  app.setGlobalPrefix('api');
-
   await app.listen(3000);
-  console.log(`🚀 Party API running at http://localhost:3000/api/health`);
 }
 bootstrap();
