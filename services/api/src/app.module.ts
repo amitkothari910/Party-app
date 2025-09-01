36782fef8b02abf7098e66391121267c55f92f9a
Amit Kothari <amitkothari910@gmail.com>
2025-09-02 03:18:21 +0530

    Update app.module.ts

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
 import { PartiesModule } from './parties/parties.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HealthModule, UsersModule, PartiesModule],
+  imports: [HealthModule, UsersModule, PartiesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
