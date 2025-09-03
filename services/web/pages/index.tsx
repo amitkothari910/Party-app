45dd7bd8a0ec09db694ea8a44423b33bf74dbddd
Amit Kothari <amitkothari910@gmail.com>
2025-09-03 11:53:33 +0530

    Create index.tsx

---
+++ services/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎉 Welcome to Party App 🎉</h1>
+      <p>Frontend is running with Next.js 🚀</p>
+    </div>
+  );
+}
